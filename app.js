4395bc3fba1673aca4058e6a68fc29b9330becfc
Karyme <amery2179@gmail.com>
2024-10-22 18:05:29 -0600

    contador

---
+++ app.js
@@ -0,0 +1,89 @@
+document.addEventListener('DOMContentLoaded', function(){
+    agregarManzana();
+    agregarPera();
+    agregarDurazno();
+    
+    contarFrutas();
+
+    limpiar();
+});
+
+
+function agregarManzana() {
+    const manzana = document.querySelector('#manzana');
+
+    manzana.addEventListener('click', function(){
+        let contenido = document.getElementById("frutas").value;
+        document.getElementById("frutas").value = contenido + " Manzana";
+    });
+}
+
+function agregarPera(){
+    const pera = document.querySelector('#pera');
+
+    pera.addEventListener('click', function(){
+
+        let contenido = document.getElementById("frutas").value;
+        document.getElementById("frutas").value = contenido + " Pera";
+    });
+}
+
+
+function agregarDurazno(){
+    const durazno = document.querySelector('#durazno');
+
+    durazno.addEventListener('click', function(){
+
+        let contenido = document.getElementById("frutas").value;
+        document.getElementById("frutas").value = contenido + " Durazno";
+    });
+}
+
+function contarFrutas() {
+    const contar = document.querySelector('#contar');
+
+    contar.addEventListener('click', function(){
+        let contenido = document.getElementById('frutas').value;
+
+        // Limpiar espacios
+        contenido = contenido.trim();
+        frutas = contenido.split(' ');
+        console.log(frutas);
+
+        let manzanas = 0;
+        let peras = 0;
+        let duraznos = 0;
+
+        frutas.forEach(fruta => {
+            switch(fruta){
+                case "Manzana":
+                    manzanas += 1;
+                    break;
+                case "Pera":
+                    peras += 1;
+                    break;
+                case "Durazno":
+                    duraznos += 1;
+                    break;
+            }
+        });
+        
+        // Crear etiqueta        
+        const contenedor = document.getElementById('contenedor');
+        let label = document.createElement('LABEL');
+        label.innerHTML = 'Hay ' + manzanas + ' manzanas, ' + peras + ' peras, ' + duraznos + ' duraznos';
+        
+        // Mostrar resultado         
+        contenedor.appendChild(label);
+
+    });
+}
+
+function limpiar() {
+    const contar = document.querySelector('#limpiar');
+
+    contar.addEventListener('click', function(){
+        const frutas = document.getElementById('frutas');
+        document.getElementById("frutas").value = "";
+    })
+}
